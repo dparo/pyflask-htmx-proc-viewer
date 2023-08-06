63f4486242afc31f3973ba1be0f7789eff881c67
Davide Paro <dparo@outlook.it>
2023-08-06 09:42:37 +0200

    Added command runner and proc list refresh.

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, render_template_string, send_from_directory
 import os
-from dataclasses import dataclass
-from markupsafe import escape
 import signal
-from flask import request, send_from_directory
+import subprocess
+from dataclasses import dataclass
+
+from flask import (Flask, make_response, render_template,
+                   render_template_string, request, send_from_directory)
 
 app = Flask(__name__)
 
@@ -14,29 +15,44 @@ class Proc:
     argv: str
 
 
-
-@app.route('/favicon.ico')
+@app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 
 
-@app.route("/api/pid/<int:id>", methods=['DELETE'])
+@app.delete("/api/pid/<int:id>")
 def kill_pid(id: int):
-    os.kill(id, signal.SIGINT)
-    return ""
+    sigstr = request.args["signal"] or "SIGINT"
+    print(f"Killing {id} with signal = {sigstr}")
+    os.kill(id, signal.Signals[sigstr].value)
+    response = make_response("")
+    response.headers["HX-Trigger"] = "evProcListRefresh"
+    return response
+
+
+@app.post("/api/cmd")
+def new_cmd():
+    subprocess.Popen(request.form["cmd"], shell=True)
+    response = make_response("")
+    response.headers["HX-Trigger"] = "evProcListRefresh"
+    return response
+
 
 @app.route("/api/procs")
 def get_procs():
-    pids = list(filter(lambda x: x.isnumeric(), list(os.listdir('/proc'))))
+    pids = list(filter(lambda x: x.isnumeric(), list(os.listdir("/proc"))))
     pids = [int(x) for x in pids]
 
     cmds = []
     for p in pids:
         try:
-            with open(f'/proc/{p}/cmdline', 'r') as f:
+            with open(f"/proc/{p}/cmdline", "r") as f:
                 argv = f.read()
-                argv = argv.replace('\u0000', ' ').strip()
+                argv = argv.replace("\u0000", " ").strip()
                 cmds.append(Proc(p, argv))
         except Exception:
             pass
@@ -46,10 +62,33 @@ def get_procs():
         "cmds": cmds,
     }
 
+
+@app.get("/procs")
+def html_procs():
+    procs = get_procs()
+    return render_template_string(
+        """
+        <ul class="list-disc" hx-get="/procs" hx-trigger="evProcListRefresh from:body">
+            {% for cmd in cmds %}
+                <li id="li-{{ cmd.pid }}">
+                    {% for signal in ['SIGTERM', 'SIGINT', 'SIGKILL'] %}
+                    <button hx-delete="/api/pid/{{ cmd.pid }}?signal={{ signal }}" hx-swap="outerHTML" hx-target="closest li"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ signal }}</button>
+                    {% endfor %}
+                    {{ cmd.pid }} - {{ cmd.argv }}
+
+                </li>
+            {% endfor %}
+        </ul>
+        """,
+        cmds=procs["cmds"],
+    )
+
+
 @app.route("/")
 def index_route():
-    procs = get_procs()
+    proc_list = html_procs()
     refresh_time_msecs = 10000 * 1000
-    return render_template('index.xhtml',
-                           cmds=procs['cmds'],
-                           refresh_time_msecs=refresh_time_msecs)
+    return render_template(
+        "index.xhtml", html_proc_list=proc_list, refresh_time_msecs=refresh_time_msecs
+    )
